25d52f085b5415f278b1072b2e8f5146e1bb37f8
Daniel Kang <danieldonghoonkang@gmail.com>
2019-07-08 17:31:14 -0700

    completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,18 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var elementsContainingClass = [];
+
+  var findElementsContainingClass = function(element) {
+    if (element.classList && element.classList.contains(className)) {
+      elementsContainingClass.push(element);
+    }
+    if (element.childNodes) {
+      _.each(element.childNodes, function(item) {
+        findElementsContainingClass(item);
+      });
+    }
+  }
+  findElementsContainingClass(document.body);
+  return elementsContainingClass;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,35 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var stringify = '';
+
+  if (typeof obj === 'number'|| typeof obj === 'boolean'){
+    stringify+= obj;
+  }
+  if (obj === null){
+    return 'null';
+  }
+  if (typeof obj === 'string'){
+    obj = '"' + obj + '"';
+    stringify+= obj;
+  }
+  if (Array.isArray(obj) && obj.length === 0){
+    return stringify+= '[]';
+  }
+  if (Array.isArray(obj) && obj.length > 0){
+    var finalArray = []
+    obj.forEach(element => {
+      finalArray.push(stringifyJSON(element));
+    });
+    return stringify+= '[' + finalArray.join(',') + ']';
+  }
+  if (typeof obj === 'object'){
+    for (var key in obj){
+      if(obj[key] !== undefined && typeof obj[key] !== 'function'){
+        stringify+= stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    return '{' + stringify.slice(0,stringify.length-1) + '}';
+  }
+  return stringify;
 };
